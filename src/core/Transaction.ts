f76639560bfbdbc0c061ab58a3241b9ca584d69c
André Teixeira <andremarcondesteixeira@gmail.com>
2022-08-28 17:10:27 -0300

    transaction validation

--- src/core/Transaction.ts
+++ src/core/Transaction.ts
@@ -1,16 +1,6 @@
 import type { Account } from "./Account";
 import type { Money } from "./Money";
 
-export type TransactionComponent = {
-    account: Account;
-    amount: Money;
-};
-
-export type TransactionConstructorProps = {
-    credits: TransactionComponent[];
-    debits: TransactionComponent[];
-};
-
 export class Transaction {
     private _credits: TransactionComponent[];
     private _debits: TransactionComponent[];
@@ -27,4 +17,46 @@ export class Transaction {
     get debits() {
         return [...this._debits];
     }
+
+    validate() {
+        const sameCurrency = [...this._credits, ...this._debits].every((transactionComponent, _, creditsAndDebits) => {
+            return transactionComponent.amount.currency === creditsAndDebits[0]?.amount.currency;
+        });
+
+        if (!sameCurrency) {
+            return {
+                isValid: false,
+                error: TransactionValidationError.DIFFERENT_CURRENCIES,
+            };
+        }
+
+        const sumOfCredits = this._credits.reduce((sum, transactionComponent) => {
+            return sum + transactionComponent.amount.value;
+        }, 0);
+        const sumOfDebits = this._debits.reduce((sum, transactionComponent) => {
+            return sum + transactionComponent.amount.value;
+        }, 0);
+
+        const isValid = sumOfCredits === sumOfDebits;
+
+        return {
+            isValid,
+            error: isValid ? undefined : TransactionValidationError.SUM_OF_CREDITS_DIFFERENT_THAN_SUM_OF_DEBITS,
+        };
+    }
+}
+
+export type TransactionComponent = {
+    account: Account;
+    amount: Money;
+};
+
+export type TransactionConstructorProps = {
+    credits: TransactionComponent[];
+    debits: TransactionComponent[];
+};
+
+export enum TransactionValidationError {
+    DIFFERENT_CURRENCIES = "Attempt to use different currencies in the same transaction",
+    SUM_OF_CREDITS_DIFFERENT_THAN_SUM_OF_DEBITS = "The sum of credits is not equal to the sum of debits",
 }
--- test/core/Transaction.spec.ts
+++ test/core/Transaction.spec.ts
@@ -1,7 +1,7 @@
 import { Account } from "../../src/core/Account";
 import { AccountingNature } from "../../src/core/AccountingNature";
 import { Money } from "../../src/core/Money";
-import { Transaction } from "../../src/core/Transaction";
+import { Transaction, TransactionValidationError } from "../../src/core/Transaction";
 
 describe("Transaction component", () => {
     test("A Transaction component can be created", () => {
@@ -35,5 +35,68 @@ describe("Transaction component", () => {
         expect(transaction.credits[0]?.amount).toBe(money);
         expect(transaction.debits[0]?.account).toBe(bankAccount);
         expect(transaction.debits[0]?.amount).toBe(money);
+    });
+
+    test("The amount of credits must be equal to the amount of debits", () => {
+        const transaction = new Transaction({
+            credits: [
+                {
+                    account: new Account({
+                        name: "A",
+                        nature: AccountingNature.CREDIT,
+                    }),
+                    amount: new Money({
+                        value: 100,
+                        currency: "BRL",
+                    }),
+                },
+            ],
+            debits: [
+                {
+                    account: new Account({
+                        name: "B",
+                        nature: AccountingNature.DEBIT,
+                    }),
+                    amount: new Money({
+                        value: 120,
+                        currency: "BRL",
+                    }),
+                },
+            ],
+        });
+        const validationResult = transaction.validate();
+        expect(validationResult.isValid).toBe(false);
+        expect(validationResult.error).toEqual(TransactionValidationError.SUM_OF_CREDITS_DIFFERENT_THAN_SUM_OF_DEBITS);
+    });
+
+    test("Currencies must be the same", () => {
+        const transaction = new Transaction({
+            credits: [
+                {
+                    account: new Account({
+                        name: "A",
+                        nature: AccountingNature.CREDIT,
+                    }),
+                    amount: new Money({
+                        value: 100,
+                        currency: "BRL",
+                    }),
+                },
+            ],
+            debits: [
+                {
+                    account: new Account({
+                        name: "B",
+                        nature: AccountingNature.DEBIT,
+                    }),
+                    amount: new Money({
+                        value: 100,
+                        currency: "USD",
+                    }),
+                },
+            ],
+        });
+        expect(transaction.validate().isValid).toBe(false);
+        expect(transaction.validate().error).toEqual(TransactionValidationError.DIFFERENT_CURRENCIES);
     });
 });
